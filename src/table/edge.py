5c384c291376ffbc4abcaf305ef843608bba2eb7
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-02-27 15:26:36 +0100

    add table edge

---
+++ src/table/edge.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Edge:
+    LEFTTOP="╔"
+    RIGHTTOP="╗"
+    LEFTBOTTOM="╚"
+    RIGHTBOTTOM="╝"
+    BOTTOMTOP="═"
+    LEFTRIGHT="║"
+    TOPMIDDLE="╦"
+    BOTTOMMIDDLE="╩"
